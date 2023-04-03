07bd5e012fc7f20cba76a8df92a559baef3c286c
SebastianZasadni <emblelele@gmail.com>
2023-04-03 20:50:18 +0200

    fix task-01

--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -3,14 +3,12 @@ import { FeedbackOptions } from 'components/feedbackoptions/FeedbackOptions';
 import './Section.css';
 
 export const Section = ({ title }) => {
-  {
-    return (
-      <div className="section-feedback">
-        <h2>{title}</h2>
-        <FeedbackOptions />
-      </div>
-    );
-  }
+  return (
+    <div className="section-feedback">
+      <h2>{title}</h2>
+      <FeedbackOptions />
+    </div>
+  );
 };
 
 Section.propTypes = {
