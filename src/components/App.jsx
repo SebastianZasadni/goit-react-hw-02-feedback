300168836a1dc697a48e57a242dfb5ea49effc88
SebastianZasadni <emblelele@gmail.com>
2023-04-08 18:27:32 +0200

    fix task-02

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import { Section } from './feedback/section/Section.jsx';
-
+import { Phonebook } from './phonebook/Phonebook.jsx';
 export const App = () => (
   <>
     <Section title="Please leave feedback" />
+    <Phonebook/>
   </>
 );
