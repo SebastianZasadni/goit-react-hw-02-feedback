c35624950fd123849b609e90db914537478139a3
SebastianZasadni <emblelele@gmail.com>
2023-05-11 11:11:15 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { Section } from './components/feedback/Section/Section.jsx';
-import { Phonebook } from './components/phonebook/Phonebook.jsx';
+import { Section } from './components/Feedback/Section/Section.jsx';
+import { Phonebook } from './components/Phonebook/Phonebook.jsx';
 export const App = () => (
   <>
     <Section title="Please leave feedback" />
